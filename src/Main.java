cb1c00a4901d64489babdec5e1cd578f330578cf
protojaime <33036535+protojaime@users.noreply.github.com>
2019-05-25 16:58:20 +0200

    commented introspector

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ import ast.Program;
 import codegeneration.CodeGenerator;
 import codegeneration.ExecuteVisitor;
 import codegeneration.OffsetVisitor;
+import introspector.model.IntrospectorModel;
+import introspector.view.IntrospectorTree;
 import parser.PmmLexer;
 import parser.PmmParser;
 import semantic.EvalVisitor;
@@ -47,8 +49,8 @@ public class Main {
 		} else {
 
 			// * The AST is shown
-			// IntrospectorModel model = new IntrospectorModel("Program", ast);
-			// new IntrospectorTree("Introspector", model);
+			IntrospectorModel model = new IntrospectorModel("Program", ast);
+			new IntrospectorTree("Introspector", model);
 
 			// code generation section
 			PrintStream output = new PrintStream(args[1]);
